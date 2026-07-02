77bca033f727ad23591d0f03043e065a067643c1
xgx-jrz <2986707370@qq.com>
2026-07-02 14:33:40 +0000

    首页接入商品列表接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,15 @@
+const api = require('../../services/api');
+const util = require('../../utils/common');
+
 Page({
   data: {
-    goodsList: [
-      { id: 1, title: '大学英语教材', price: 25, desc: '九成新，适合大一新生' },
-      { id: 2, title: '机械键盘', price: 180, desc: '蓝牙连接，使用良好' }
-    ]
+    goodsList: []
+  },
+  onLoad() {
+    this.loadGoods();
+  },
+  async loadGoods() {
+    const res = await api.getGoodsList();
+    this.setData({ goodsList: res.data });
   }
 });
