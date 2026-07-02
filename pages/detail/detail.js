2017fd93c5b3647f293ae73da4960bfd1d15fd3c
xgx-jrz <2986707370@qq.com>
2026-07-02 14:46:20 +0000

    详情页接收商品id参数

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,9 +1,6 @@
 Page({
-  data: {
-    detail: {
-      title: '校园书包',
-      price: 80,
-      desc: '使用一年，容量够大，适合学生党'
-    }
+  data: {},
+  onLoad(options) {
+    console.log('商品id', options.id);
   }
 });
