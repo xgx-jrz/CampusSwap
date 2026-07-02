b37ebed9cc66dc0c5e7f805df0ac98f876cc727a
xgx-jrz <2986707370@qq.com>
2026-07-02 14:40:34 +0000

    补充通用工具函数

--- utils/common.js
+++ utils/common.js
@@ -1,10 +1,23 @@
-function formatTime(date) {
+// 时间格式化
+function formatTime(timestamp) {
+  const date = new Date(timestamp);
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
   return `${year}-${month}-${day}`;
 }
 
+// 本地存储封装
+function setStorage(key, val) {
+  wx.setStorageSync(key, val);
+}
+
+function getStorage(key) {
+  return wx.setStorageSync(key);
+}
+
 module.exports = {
-  formatTime
+  formatTime,
+  setStorage,
+  getStorage
 };
